8677cd6dae041c4beee1d355564a502e439d41f2
ImranMuhammad113437 <nightcore113435@gmail.com>
2024-06-21 00:44:33 +0500

    Table added

--- student.py
+++ student.py
@@ -27,9 +27,58 @@ class Student:
 
         main_frame.place(x=20,y=70,width=984,height=500)
         
-    #Left Label Frame    
+    #Left Label Frame(Student Information: Current Course,Class Student Information)    
         left_frame=LabelFrame(main_frame,bd=2,relief=RIDGE,text="Student Information")
-        left_frame.place(x=10,y=10,width=200,height=200)
+        left_frame.place(x=10,y=10,width=477,height=470)
+
+    #Current Course Information
+        current_course_frame=LabelFrame(left_frame,bd=2,relief=RIDGE,text="Current Course Information")
+        current_course_frame.place(x=5,y=5,width=462,height=100)
+
+        #Department Section
+        department_label=Label(current_course_frame,text="Department")
+        department_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        department_dropdown=ttk.Combobox(current_course_frame,state="readonly")
+        department_dropdown["values"]=("Select Department","Software Engineer")
+        department_dropdown.current(0)
+        department_dropdown.grid(row=0,column=1,pady=10,sticky=W)
+
+        #Course Section
+        course_label=Label(current_course_frame,text="Course")
+        course_label.grid(row=0,column=2,padx=10,sticky=W)
+
+        course_dropdown=ttk.Combobox(current_course_frame,state="readonly")
+        course_dropdown["values"]=("Select Course","Software")
+        course_dropdown.current(0)
+        course_dropdown.grid(row=0,column=3,pady=10,sticky=W)
+
+        #Year Section
+        year_label=Label(current_course_frame,text="Year")
+        year_label.grid(row=1,column=0,padx=10,sticky=W)
+
+        year_dropdown=ttk.Combobox(current_course_frame,state="readonly")
+        year_dropdown["values"]=("Select Year","2024","2025","2026")
+        year_dropdown.current(0)
+        year_dropdown.grid(row=1,column=1,pady=10,sticky=W)
+
+
+        #Semester Section 
+        semester_label=Label(current_course_frame,text="Semester")
+        semester_label.grid(row=1,column=2,padx=10,sticky=W)
+
+        semester_dropdown=ttk.Combobox(current_course_frame,state="readonly")
+        semester_dropdown["values"]=("Select Semester","Semester-1","Semester-2","Semester-3","Semester-4","Semester-5","Semester-6","Semester-7","Semester-8")
+        semester_dropdown.current(0)
+        semester_dropdown.grid(row=1,column=3,pady=10,sticky=W)
+
+    #Class Student Information
+        class_student_frame=LabelFrame(left_frame,bd=2,relief=RIDGE,text="Class Student Information")
+        class_student_frame.place(x=5,y=110,width=462,height=50) 
+
+    #Right Label Frame     
+        right_frame=LabelFrame(main_frame,bd=2,relief=RIDGE,text="Student Information")
+        right_frame.place(x=497,y=10,width=475,height=200)
 
 if __name__ == "__main__":
     root=Tk()
