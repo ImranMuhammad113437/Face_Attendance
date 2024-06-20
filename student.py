0e442cb199b08743bff214eacd6b559c577fd51f
ImranMuhammad113437 <nightcore113435@gmail.com>
2024-06-20 11:21:46 +0500

    update the path

--- student.py
+++ student.py
@@ -10,14 +10,14 @@ class Student:
         self.root.title("AttendNow")
 
     #Background Image
-        background_img=Image.open(r"C:\Users\night\Desktop\Face_Attendance\Image\Background.png")
+        background_img=Image.open(r"Image\Background.png")
         background_img=background_img.resize((1024, 590),Image.Resampling.LANCZOS)
         self.photo_background_img=ImageTk.PhotoImage(background_img)
         background_img_position=Label(self.root,image=self.photo_background_img)
         background_img_position.place(x=0,y=0,width=1024,height=590)
 
     #LogoTitle Image
-        left_title=Image.open(r"C:\Users\night\Desktop\Face_Attendance\Image\LogoTitle_Left Top.png")     
+        left_title=Image.open(r"Image\LogoTitle_Left Top.png")     
         self.photoleft_title=ImageTk.PhotoImage(left_title)
         left_title_position=Label(self.root,image=self.photoleft_title)
         left_title_position.place(x=0,y=0,width=163,height=60)
